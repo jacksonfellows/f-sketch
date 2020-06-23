7c78807d9d96098595c5f692e26b8dead56a0088
Jackson Fellows <jtfellows@icloud.com>
2020-06-22 22:16:21 -0700

    shape, then arguments

--- functions.js
+++ functions.js
@@ -30,11 +30,11 @@ function difference(a,b) {
     return intersection(a, inv(b));
 }
 
-function translate(dx,dy,shape) {
-    return (x,y) => shape(x-dx,y-dy);
+function translate(shape, dx, dy) {
+    return (x,y) => shape(x - dx, y - dy);
 }
 
-function rotate(theta,shape) {
+function rotate(shape, theta) {
     return (x,y) => shape(x*Math.cos(theta) - y*Math.sin(theta), x*Math.sin(theta) + y*Math.cos(theta));
 }
 
