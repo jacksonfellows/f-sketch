12ad8a2948ce80a35e2f538ea42baccae761ebe4
Jackson Fellows <jtfellows@icloud.com>
2020-06-26 00:15:57 -0700

    add 3-point circle and segment

--- functions.js
+++ functions.js
@@ -2,6 +2,35 @@ function circle(r, x0, y0) {
     return (x,y) => Math.sqrt((x0 - x)**2 + (y0 - y)**2) - r;
 }
 
+function threePointCircle(x0, y0, x1, y1, x2, y2) {
+	var A = x0*(y1 - y2) - y0*(x1 - x2) + x1*y2 - x2*y1;
+	var B = (x0**2 + y0**2)*(y2 - y1) + (x1**2 + y1**2)*(y0 - y2) + (x2**2 + y2**2)*(y1 - y0);
+	var C = (x0**2 + y0**2)*(x1 - x2) + (x1**2 + y1**2)*(x2 - x0) + (x2**2 + y2**2)*(x0 - x1);
+	var D = (x0**2 + y0**2)*(x2*y1 - x1*y2) + (x1**2 + y1**2)*(x0*y2 - x2*y0) + (x2**2 + y2**2)*(x1*y0 - x0*y1);
+
+	var centerX = -B/(2*A);
+	var centerY = -C/(2*A);
+	var r = Math.sqrt((B**2 + C**2 - 4*A*D)/(4*A**2));
+
+	return circle(r, centerX, centerY);
+}
+
+function half(x0, y0, x1, y1, x2, y2) {
+	var line;
+	if (x0 == x1) {
+		line = (x,y) => x - x0;
+	} else {
+		var m = (y1 - y0)/(x1 - x0);
+		line = (x,y) => x*m + y0 - x0*m - y;
+	}
+
+	return line(x2,y2) < 0 ? line : inv(line);
+}
+
+function threePointSegment(x0, y0, x1, y1, x2, y2) {
+	return intersection(threePointCircle(x0, y0, x1, y1, x2, y2), half(x0, y0, x2, y2, x1, y1));
+}
+
 function left(x0) {
     return (x,y) => x - x0;
 }
