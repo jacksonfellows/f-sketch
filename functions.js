37e3167612dcb8b6ab43e9500c1620dc73f4ae6f
Jackson Fellows <jtfellows@icloud.com>
2020-06-27 20:59:09 -0700

    use tabs!

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function circle(r, x0, y0) {
-    return (x,y) => Math.sqrt((x0 - x)**2 + (y0 - y)**2) - r;
+	return (x,y) => Math.sqrt((x0 - x)**2 + (y0 - y)**2) - r;
 }
 
 function threePointCircle(x0, y0, x1, y1, x2, y2) {
@@ -40,23 +40,23 @@ function tri(x0, y0, x1, y1, x2, y2) {
 }
 
 function left(x0) {
-    return (x,y) => x - x0;
+	return (x,y) => x - x0;
 }
 
 function right(x0) {
-    return (x,y) => x0 - x;
+	return (x,y) => x0 - x;
 }
 
 function lower(y0) {
-    return (x,y) => y - y0;
+	return (x,y) => y - y0;
 }
 
 function upper(y0) {
-    return (x,y) => y0 - y;
+	return (x,y) => y0 - y;
 }
 
 function binUnion(a,b) {
-    return (x,y) => Math.min(a(x,y), b(x,y));
+	return (x,y) => Math.min(a(x,y), b(x,y));
 }
 
 function union(...args) {
@@ -64,7 +64,7 @@ function union(...args) {
 }
 
 function binIntersection(a,b) {
-    return (x,y) => Math.max(a(x,y), b(x,y));
+	return (x,y) => Math.max(a(x,y), b(x,y));
 }
 
 function intersection(...args) {
@@ -72,23 +72,23 @@ function intersection(...args) {
 }
 
 function difference(a,...rest) {
-    return intersection(a, inv(union(...rest)));
+	return intersection(a, inv(union(...rest)));
 }
 
 function translate(shape, dx, dy) {
-    return (x,y) => shape(x - dx, y - dy);
+	return (x,y) => shape(x - dx, y - dy);
 }
 
 function rotate(shape, theta) {
-    return (x,y) => shape(x*Math.cos(theta) - y*Math.sin(theta), x*Math.sin(theta) + y*Math.cos(theta));
+	return (x,y) => shape(x*Math.cos(theta) - y*Math.sin(theta), x*Math.sin(theta) + y*Math.cos(theta));
 }
 
 function inv(shape) {
-    return (x,y) => -shape(x,y);
+	return (x,y) => -shape(x,y);
 }
 
 function rect(x0, y0, x1, y1) {
-    return intersection(right(x0), left(x1), upper(y0), lower(y1));
+	return intersection(right(x0), left(x1), upper(y0), lower(y1));
 }
 
 function roundedRect(x0, y0, x1, y1, r) {
@@ -103,17 +103,17 @@ function roundedRect(x0, y0, x1, y1, r) {
 }
 
 function scale(shape, sx, sy) {
-    return (x, y) => shape(x/sx, y/sy);
+	return (x, y) => shape(x/sx, y/sy);
 }
 
 function mirrorX(shape){
-    return scale(shape, 1, -1);
+	return scale(shape, 1, -1);
 }
 
 function mirrorY(shape) {
-    return scale(shape, -1, 1);
+	return scale(shape, -1, 1);
 }
 
 function blend(a, b, m) {
-    return (x,y) => -Math.log(Math.exp(-1/m * a(x,y)) + Math.exp(-1/m * b(x,y))) * m;
+	return (x,y) => -Math.log(Math.exp(-1/m * a(x,y)) + Math.exp(-1/m * b(x,y))) * m;
 }
--- render.js
+++ render.js
@@ -1,50 +1,50 @@
 var canvas, ctx;
 
 var DEFAULT_CONFIG = {
-    // general:
-    scaleX: 100,
-    scaleY: 100,
-    // marching squares:
-    gridDx: 5,
-    gridDy: 5
+	// general:
+	scaleX: 100,
+	scaleY: 100,
+	// marching squares:
+	gridDx: 5,
+	gridDy: 5
 };
 var CONFIG = Object.assign({}, DEFAULT_CONFIG); // shallow copy
 
 var shapesToRender = []
 
 function render(shape) {
-    shapesToRender.push(shape);
+	shapesToRender.push(shape);
 }
 
 function doRender() {
-    // drawMarchingSquares(union(...shapesToRender));
-    shapesToRender.forEach(drawMarchingSquares);
+	// drawMarchingSquares(union(...shapesToRender));
+	shapesToRender.forEach(drawMarchingSquares);
 }
 
 function resizeCanvas() {
-    canvas.width = model.clientWidth - 1;
-    canvas.height = model.clientHeight - 1;
+	canvas.width = model.clientWidth - 1;
+	canvas.height = model.clientHeight - 1;
 }
 
 window.addEventListener('resize', _ => {resizeCanvas(); doRender();}, false);
 
 function setup() {
-    canvas = document.getElementById('c');
-    ctx = canvas.getContext('2d');
+	canvas = document.getElementById('c');
+	ctx = canvas.getContext('2d');
 
 
-    model = document.getElementById('model');
-    model.oninput = function () {
-        CONFIG = Object.assign({}, DEFAULT_CONFIG); // shallow copy
+	model = document.getElementById('model');
+	model.oninput = function () {
+		CONFIG = Object.assign({}, DEFAULT_CONFIG); // shallow copy
 
-        shapesToRender = []
-        eval(model.value);
+		shapesToRender = []
+		eval(model.value);
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        doRender();
-    };
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		doRender();
+	};
 
-    resizeCanvas();
+	resizeCanvas();
 }
 
 window.onload = setup;
@@ -53,52 +53,52 @@ var toWorldX = x => (x - 0.5*canvas.width) / CONFIG.scaleX;
 var toWorldY = y => (0.5*canvas.height - y) / CONFIG.scaleY;
 
 function drawBitmap(shape) {
-    var imageData = ctx.createImageData(canvas.width, canvas.height);
-    for (var x = 0; x < canvas.width; ++x) {
-        for (var y = 0; y < canvas.height; ++y) {
-            var c = shape(toWorldX(x),toWorldY(y)) < 0 ? 0 : 255;
-            var red = y * (canvas.width * 4) + x * 4;
-            imageData.data[red]   = c;
-            imageData.data[red+1] = c;
-            imageData.data[red+2] = c;
-            imageData.data[red+3] = 255;
-        }
-    }
-    ctx.putImageData(imageData, 0, 0);
+	var imageData = ctx.createImageData(canvas.width, canvas.height);
+	for (var x = 0; x < canvas.width; ++x) {
+		for (var y = 0; y < canvas.height; ++y) {
+			var c = shape(toWorldX(x),toWorldY(y)) < 0 ? 0 : 255;
+			var red = y * (canvas.width * 4) + x * 4;
+			imageData.data[red]   = c;
+			imageData.data[red+1] = c;
+			imageData.data[red+2] = c;
+			imageData.data[red+3] = 255;
+		}
+	}
+	ctx.putImageData(imageData, 0, 0);
 }
 
 function drawGradient(shape, scale, base) {
-    // TODO: bad bad bad bad
-    var imageData = ctx.createImageData(canvas.width, canvas.height);
-    var minV = Number.POSITIVE_INFINITY;
-    var maxV = Number.NEGATIVE_INFINITY;
-    for (var x = 0; x < canvas.width; ++x) {
-        for (var y = 0; y < canvas.height; ++y) {
-            var v = shape(toWorldX(x),toWorldY(y));
-            if (v < minV) {
-                minV = v;
-            }
-            if (v > maxV) {
-                maxV = v;
-            }
-        }
-    }
-    for (var x = 0; x < canvas.width; ++x) {
-        for (var y = 0; y < canvas.height; ++y) {
-            var v = shape(toWorldX(x),toWorldY(y));
-            if (scale === 'log') {
-                var c = 255 * Math.log(v - minV + 1)/Math.log(maxV - minV + 1);
-            } else {
-                var c = 255 * (v - minV)/(maxV - minV);
-            }
-            var red = y * (canvas.width * 4) + x * 4;
-            imageData.data[red]   = c;
-            imageData.data[red+1] = c;
-            imageData.data[red+2] = c;
-            imageData.data[red+3] = 255;
-        }
-    }
-    ctx.putImageData(imageData, 0, 0);
+	// TODO: bad bad bad bad
+	var imageData = ctx.createImageData(canvas.width, canvas.height);
+	var minV = Number.POSITIVE_INFINITY;
+	var maxV = Number.NEGATIVE_INFINITY;
+	for (var x = 0; x < canvas.width; ++x) {
+		for (var y = 0; y < canvas.height; ++y) {
+			var v = shape(toWorldX(x),toWorldY(y));
+			if (v < minV) {
+				minV = v;
+			}
+			if (v > maxV) {
+				maxV = v;
+			}
+		}
+	}
+	for (var x = 0; x < canvas.width; ++x) {
+		for (var y = 0; y < canvas.height; ++y) {
+			var v = shape(toWorldX(x),toWorldY(y));
+			if (scale === 'log') {
+				var c = 255 * Math.log(v - minV + 1)/Math.log(maxV - minV + 1);
+			} else {
+				var c = 255 * (v - minV)/(maxV - minV);
+			}
+			var red = y * (canvas.width * 4) + x * 4;
+			imageData.data[red]   = c;
+			imageData.data[red+1] = c;
+			imageData.data[red+2] = c;
+			imageData.data[red+3] = 255;
+		}
+	}
+	ctx.putImageData(imageData, 0, 0);
 }
 
 var N = 0;
@@ -107,86 +107,86 @@ var S = 2;
 var W = 3;
 
 var edges = [
-    null,
-    [W,S],
-    [S,E],
-    [W,E],
-    [N,E],
-    null, // saddle
-    [N,S],
-    [W,N],
-    [W,N],
-    [N,S],
-    null, // saddle
-    [N,E],
-    [W,E],
-    [S,E],
-    [W,S],
-    null
+	null,
+	[W,S],
+	[S,E],
+	[W,E],
+	[N,E],
+	null, // saddle
+	[N,S],
+	[W,N],
+	[W,N],
+	[N,S],
+	null, // saddle
+	[N,E],
+	[W,E],
+	[S,E],
+	[W,S],
+	null
 ];
 
 function lerp(x0, y0, x1, y1, x) {
-    return y0 + (x - x0) * ((y1 - y0) / (x1 - x0));
+	return y0 + (x - x0) * ((y1 - y0) / (x1 - x0));
 }
 
 function getDx(side, sw, se, ne, nw) {
-    switch (side) {
-    case N:
-        return lerp(nw, -1, ne, 0, 0);
-    case E:
-        return 0;
-    case S:
-        return lerp(sw, -1, se, 0, 0);
-    case W:
-        return -1;
-    }
+	switch (side) {
+	case N:
+		return lerp(nw, -1, ne, 0, 0);
+	case E:
+		return 0;
+	case S:
+		return lerp(sw, -1, se, 0, 0);
+	case W:
+		return -1;
+	}
 }
 
 function getDy(side, sw, se, ne, nw) {
-    switch (side) {
-    case N:
-        return -1;
-    case E:
-        return lerp(ne, -1, se, 0, 0);
-    case S:
-        return 0;
-    case W:
-        return lerp(nw, -1, sw, 0, 0);
-    }
+	switch (side) {
+	case N:
+		return -1;
+	case E:
+		return lerp(ne, -1, se, 0, 0);
+	case S:
+		return 0;
+	case W:
+		return lerp(nw, -1, sw, 0, 0);
+	}
 }
 
 function drawMarchingSquares(shape) {
-    ctx.strokeStyle = 'red';
+	ctx.strokeStyle = 'red';
 
-    var cornersWidth = Math.floor(canvas.width / CONFIG.gridDx) + 1;
-    var cornersHeight = Math.floor(canvas.height / CONFIG.gridDy) + 1;
-    var row = new Float64Array(cornersWidth);
-    var lastRow, lastVal;
-    for (var j = 0; j < cornersHeight; ++j) {
-        for (var i = 0; i < cornersWidth; ++i) {
-            var x = i * CONFIG.gridDx;
-            var y = j * CONFIG.gridDy;
-            var val = shape(toWorldX(x), toWorldY(y));
+	var cornersWidth = Math.floor(canvas.width / CONFIG.gridDx) + 1;
+	var cornersHeight = Math.floor(canvas.height / CONFIG.gridDy) + 1;
+	var row = new Float64Array(cornersWidth);
+	var lastRow, lastVal;
+	for (var j = 0; j < cornersHeight; ++j) {
+		for (var i = 0; i < cornersWidth; ++i) {
+			var x = i * CONFIG.gridDx;
+			var y = j * CONFIG.gridDy;
+			var val = shape(toWorldX(x), toWorldY(y));
 
-            // ctx.beginPath();
-            // ctx.arc(i * CONFIG.gridDx, j * CONFIG.gridDy, 1, 2 * Math.PI, false);
-            // ctx.fillStyle = val < 0 ? 'red' : 'black';
-            // ctx.fill();
+			// ctx.beginPath();
+			// ctx.arc(i * CONFIG.gridDx, j * CONFIG.gridDy, 1, 2 * Math.PI, false);
+			// ctx.fillStyle = val < 0 ? 'red' : 'black';
+			// ctx.fill();
 
-            if (i > 0 && j > 0) {
-                var cellI = ((lastRow[i-1] < 0) << 3) | ((lastRow[i] < 0) << 2) | ((val < 0) << 1) | (lastVal < 0);
-                var edge = edges[cellI];
-                if (edge) {
-                    ctx.beginPath();
-                    ctx.moveTo(CONFIG.gridDx * (i + getDx(edge[0], lastVal, val, lastRow[i], lastRow[i-1])), CONFIG.gridDy * (j + getDy(edge[0], lastVal, val, lastRow[i], lastRow[i-1])));
-                    ctx.lineTo(CONFIG.gridDx * (i + getDx(edge[1], lastVal, val, lastRow[i], lastRow[i-1])), CONFIG.gridDy * (j + getDy(edge[1], lastVal, val, lastRow[i], lastRow[i-1])));
-                    ctx.stroke();
-                }
-            }
+			if (i > 0 && j > 0) {
+				var cellI = ((lastRow[i-1] < 0) << 3) | ((lastRow[i] < 0) << 2) | ((val < 0) << 1) | (lastVal < 0);
+				var edge = edges[cellI];
+				if (edge) {
+					ctx.beginPath();
+					ctx.moveTo(CONFIG.gridDx * (i + getDx(edge[0], lastVal, val, lastRow[i], lastRow[i-1])), CONFIG.gridDy * (j + getDy(edge[0], lastVal, val, lastRow[i], lastRow[i-1])));
+					ctx.lineTo(CONFIG.gridDx * (i + getDx(edge[1], lastVal, val, lastRow[i], lastRow[i-1])), CONFIG.gridDy * (j + getDy(edge[1], lastVal, val, lastRow[i], lastRow[i-1])));
+					ctx.stroke();
+				}
+			}
 
-            lastVal = val;
-            row[i] = val;
-        }
-        lastRow = new Float64Array(row);
-    }
+			lastVal = val;
+			row[i] = val;
+		}
+		lastRow = new Float64Array(row);
+	}
 }
